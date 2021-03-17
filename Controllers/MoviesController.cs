9d0f56223cdd59b53c9d0a344cc45479b476a3d1
DavidVictorvV <davidvictorvanvelden@gmail.com>
2021-03-17 09:48:52 +0200

    Add project files.

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MovieProjectAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private static List<Models.Movie> movies = new List<Models.Movie>()
+        {
+            new Models.Movie() { ID = Guid.NewGuid(), MovieName = "StarWars" ,Genre = "Action",MovieLength = 120, ReleaseDate = 2002},
+            new Models.Movie() { ID = Guid.NewGuid(), MovieName = "The Mandalorian" ,Genre = "Action",MovieLength = 120, ReleaseDate = 2019},
+            new Models.Movie() { ID = Guid.NewGuid(), MovieName = "Thor" ,Genre = "Comedy",MovieLength = 120, ReleaseDate = 2016 },
+            new Models.Movie() { ID = Guid.NewGuid(), MovieName = "Harry Potter" ,Genre = "Action, Magic",MovieLength = 120, ReleaseDate = 2007 },
+            new Models.Movie() { ID = Guid.NewGuid(), MovieName = "IT" ,Genre = "Horor",MovieLength = 120, ReleaseDate = 2015 }
+        };
+
+        [HttpGet]
+        public Models.Movie[] Get()
+        {
+            return movies.ToArray();
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Models.Movie duckbill)
+        {
+            if (duckbill.ID == Guid.Empty)
+                duckbill.ID = Guid.NewGuid();
+
+            movies.Add(duckbill);
+        }
+
+        [HttpPut]
+        public void Put([FromBody] Models.Movie duckbill)
+        {
+            Models.Movie currentDuckbill = movies.FirstOrDefault(x => x.ID == duckbill.ID);
+            currentDuckbill.MovieName = duckbill.MovieName;
+        }
+
+        [HttpDelete]
+        public void Delete(Guid id)
+        {
+            movies.RemoveAll(duckbill => duckbill.ID == id);
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieProjectAPI.Models
+{
+    public class Movie
+    {
+        public Guid ID { get; set; }
+
+        public string MovieName { get; set; }
+
+        public string Genre { get; set; }
+
+        public int MovieLength { get; set; }
+
+        public int ReleaseDate { get; set; }
+    }
+}
